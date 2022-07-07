d99c174f9f5d002355239b7d992d5261eda02c33
PatrykKobylarczyk <patryk.kobylarczyk@gmail.com>
2022-07-07 17:40:11 +0200

    repair sections images rwd

--- src/components/Categories/Category.js
+++ src/components/Categories/Category.js
@@ -5,7 +5,7 @@ import Button from '../Buttons/Button'
 const Category = ({image, name}) => {
     return (
         <>
-            <img src={image} alt={name} />
+            <img src={image} alt={name} className='w-full'/>
             <h2 className='my-5'>{name}</h2>
             <Button name= 'Shop now' width='w-2/5' margin='mb-12'/>
         </>
