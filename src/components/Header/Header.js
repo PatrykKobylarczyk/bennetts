44ee6b2083c7e8c183fe424921ac3cd2e613b0e8
PatrykKobylarczyk <patryk.kobylarczyk@gmail.com>
2022-07-15 10:08:44 +0200

    try to repair rwd

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,14 +9,14 @@ import DesktopMenu from './DesktopMenu';
 import MobileMenu from './MobileMenu';
 import Menu from './Menu';
 
-const Header = ({ isDesktop, menuActive, setMenuActive }) => {
+const Header = ({ isTablet, isDesktop, menuActive, setMenuActive }) => {
 
     return (
         <>
             <Menu menuActive={menuActive} setMenuActive={setMenuActive} />
             <nav className='w-full h-16 lg:h-24 flex items-center justify-between px-5 lg:px-16'>
                 <Link to="/bennetts/"><img src={logo} className='w-8 lg:w-12 cursor-pointer' alt="Logo" /></Link>
-                {isDesktop
+                {isTablet
                     ? <DesktopMenu />
                     : <MobileMenu menuActive={menuActive} setMenuActive={setMenuActive} />
                 }
