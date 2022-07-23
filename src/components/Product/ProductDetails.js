07e13ff2dfa9c61a91a5cc3c889802b6e199decc
PatrykKobylarczyk <patryk.kobylarczyk@gmail.com>
2022-07-23 23:40:23 +0200

    repair detail panel width

--- src/components/Product/ProductDetails.js
+++ src/components/Product/ProductDetails.js
@@ -5,7 +5,7 @@ const ProductDetails = ({detail}) => {
         <div className='w-full p-5'>
                     <h2 className=' text-lg font-bold my-3'>{detail.title}</h2>
                     <p>{detail.description}</p>
-                    <ul className='my-5 lg:w-2/5'>
+                    <ul className='my-5'>
                         <li className='flex justify-between'><p>Brand: </p><strong className='ml-2'>{detail.brand}</strong></li>
                         <li className='flex justify-between'><p>Collection: </p><strong className='ml-2'>{detail.collection}</strong></li>
                         <li className='flex justify-between'><p>Gender: </p> <strong className='ml-2'>{detail.gender}</strong></li>
